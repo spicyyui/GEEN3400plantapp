49d019eeb77f51436a7c2b43c17d17ee10e47ed1
Graham-CO <grwi2594@colorado.edu>
2022-02-11 00:49:35 -0700

    added endpoints

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	//"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+/**************ENDPOINTS******************/
+
+// ESP32 MCU Interacts with These Values
+type pot struct {
+	PotID    string  `json:"id"`
+	PotColor []int8  `json:"potcolor"`
+	Temp     float64 `json:"temp"`
+	Water    float64 `json:"water"`
+	Light    float64 `json:"light"`
+}
+
+// iOS Device Interacts with These Values
+type user struct {
+	// Device	string	`json:"device"`
+	UserID  string `json:"id"`
+	UserRGB []int8 `json:"usercolor"`
+}
+
+/***************METHODS******************/
+
+// Sensor Data Methods
+
+// getTemp responds with the float64 temp sens value
+func getTemp(c *gin.Context) {
+	// TODO: How to retrieve specific value from JSON?
+}
